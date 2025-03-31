ba562128bcbfaf114b1969133e3045ed3c7e7d86
victor <mastervictorpolar@gmail.com>
2025-03-31 09:50:32 -0300

    enviandi exercicio 02

---
+++ atv02.py
@@ -0,0 +1,14 @@
+numero1 = float(input("primeiro numero"))
+numero2 = float(input("segundo numero"))
+
+Soma = numero1+numero2
+print(Soma)
+
+divisao= numero1/numero2
+print(divisao)
+
+multiplicaçao = numero1*numero2
+print(multiplicaçao)
+
+subtraçao = numero1-numero2
+print(subtraçao)
