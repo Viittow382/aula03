b5a45faecb4728e866e2021d8b828bfcf8cfb5d5
victor <mastervictorpolar@gmail.com>
2025-04-09 08:54:31 -0300

    Exercicio10

--- atv08.py
+++ atv08.py
@@ -1,6 +1,3 @@
-numero= float(input(f"digite seu numero aqui"))
-if numero % 2==0:
-    print(f"o {numero} é par")
 
-else:
-    print(f"o {numero} é impar")
+for x in range(1,11,1):
+    print(x,end=" ")
